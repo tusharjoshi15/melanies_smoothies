f4454613c30972a24186fbf52f09d446cc2956be
Tushar Uday Joshi <joshi.tushhar@gmail.com>
2025-12-30 14:09:06 +0000

    Add Smoothiefroot nutrition information display
    Display Smoothiefroot nutrition information in a dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,12 @@ if ingredients_list:
     
         st.success('Your Smoothie is ordered! '+ name_on_order , icon="✅")
 
+
 # New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
+
